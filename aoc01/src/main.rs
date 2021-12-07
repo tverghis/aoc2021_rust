3ab47bcdad09ed5c6bc136ee692840b355eac730
Tarun Verghis <tarun.verghis@gmail.com>
2021-12-06 20:13:10 -0800

    Day 01 Part 2

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -7,15 +7,25 @@ fn main() {
         .collect::<Vec<_>>();
 
     println!("Part 1: {}", count_increases(&input));
+    println!("Part 2: {}", count_increasing_sums(&input));
 }
 
 fn count_increases(input: &[u32]) -> usize {
     input.windows(2).filter(|nums| nums[1] > nums[0]).count()
 }
 
+fn count_increasing_sums(input: &[u32]) -> usize {
+    let three_window_sums = input
+        .windows(3)
+        .map(|nums| nums.iter().sum())
+        .collect::<Vec<u32>>();
+
+    count_increases(&three_window_sums)
+}
+
 #[cfg(test)]
 mod test {
-    use super::count_increases;
+    use super::*;
 
     #[test]
     fn test_count_increases() {
@@ -26,6 +36,20 @@ mod test {
         assert_eq!(
             count_increases(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
             7
+        );
+    }
+
+    #[test]
+    fn test_count_increasing_sums() {
+        assert_eq!(count_increasing_sums(&[]), 0);
+        assert_eq!(count_increasing_sums(&[1]), 0);
+        assert_eq!(count_increasing_sums(&[1, 2]), 0);
+        assert_eq!(count_increasing_sums(&[1, 2, 3]), 0);
+        assert_eq!(count_increasing_sums(&[1, 2, 3, 0]), 0);
+        assert_eq!(count_increasing_sums(&[1, 2, 3, 1]), 0);
+        assert_eq!(
+            count_increasing_sums(&[199, 200, 208, 210, 200, 207, 240, 269, 260, 263]),
+            5
         );
     }
 }
