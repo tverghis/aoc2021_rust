db7e398d28467ed3133278046245723834608124
Tarun Verghis <tarun.verghis@gmail.com>
2021-12-26 17:54:57 -0600

    Refactor into fish_population_after_days

--- aoc06/src/main.rs
+++ aoc06/src/main.rs
@@ -5,10 +5,19 @@ fn main() {
         .map(|n| n.parse::<u32>().unwrap())
         .collect::<Vec<_>>();
 
-    println!("Part 1: {}", part_1(&input, 80));
+    println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
 
-fn part_1(input: &[u32], num_days: u32) -> usize {
+fn part_1(input: &[u32]) -> usize {
+    fish_population_after_days(input, 80)
+}
+
+fn part_2(input: &[u32]) -> usize {
+    fish_population_after_days(input, 256)
+}
+
+fn fish_population_after_days(input: &[u32], num_days: u32) -> usize {
     let mut input: Vec<u32> = input.iter().copied().collect();
 
     for _ in 0..num_days {
@@ -30,12 +39,12 @@ fn part_1(input: &[u32], num_days: u32) -> usize {
 
 #[cfg(test)]
 mod test {
-    use crate::part_1;
+    use super::*;
 
     #[test]
-    fn test_part_1() {
+    fn test_fish_population() {
         let input = vec![3, 4, 3, 1, 2];
-        assert_eq!(part_1(&input, 18), 26);
-        assert_eq!(part_1(&input, 80), 5934);
+        assert_eq!(fish_population_after_days(&input, 18), 26);
+        assert_eq!(fish_population_after_days(&input, 80), 5934);
     }
 }
