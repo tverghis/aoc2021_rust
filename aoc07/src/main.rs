44cb05aaf31298562644821cccfdab3e2184a7b3
Tarun Verghis <tarun.verghis@gmail.com>
2021-12-26 23:40:02 -0600

    Day 07 Part 2

--- aoc07/src/main.rs
+++ aoc07/src/main.rs
@@ -8,21 +8,40 @@ fn main() {
     input.sort_unstable();
 
     println!("Part 1: {}", part_1(&input));
+    println!("Part 2: {}", part_2(&input));
 }
 
 fn part_1(input: &[u32]) -> u32 {
-    // The median value gives us the necessary answer, because
+    // The median value gives us the necessary position, because
     // the property of the median is that it minimizes the distance
     // between the values in the set.
     // We assume that input is sorted.
     let pos = input[(input.len() + 1) / 2];
-    align_to_position(input, pos)
+    cost_linear(input, pos)
 }
 
-fn align_to_position(input: &[u32], pos: u32) -> u32 {
+fn part_2(input: &[u32]) -> u32 {
+    let mut lowest_cost = u32::MAX;
+
+    // Assume that the ideal position lies in [0, max(input)]
+    for pos in 0..(*input.iter().max().unwrap()) {
+        let total_cost = input.iter().fold(0, |acc, &x| acc + cost_nonlinear(x, pos));
+        lowest_cost = u32::min(lowest_cost, total_cost);
+    }
+
+    lowest_cost
+}
+
+fn cost_linear(input: &[u32], pos: u32) -> u32 {
     input
         .iter()
         .fold(0, |acc, x| acc + (*x as i32 - pos as i32).abs() as u32)
+}
+
+fn cost_nonlinear(cur: u32, desired: u32) -> u32 {
+    let diff = (cur as i32 - desired as i32).abs() as u32;
+    // sum of natural numbers [1, diff]
+    (diff * (diff + 1)) / 2
 }
 
 #[cfg(test)]
@@ -31,9 +50,21 @@ mod test {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(align_to_position(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 2), 37);
-        assert_eq!(align_to_position(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 1), 41);
-        assert_eq!(align_to_position(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 3), 39);
-        assert_eq!(align_to_position(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 10), 71);
+        assert_eq!(cost_linear(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 2), 37);
+        assert_eq!(cost_linear(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 1), 41);
+        assert_eq!(cost_linear(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 3), 39);
+        assert_eq!(cost_linear(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16], 10), 71);
+    }
+
+    #[test]
+    fn test_cost_nonlinear() {
+        assert_eq!(cost_nonlinear(3, 5), 3);
+        assert_eq!(cost_nonlinear(16, 5), 66);
+        assert_eq!(cost_nonlinear(5, 5), 0);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(&[0, 1, 1, 2, 2, 2, 4, 7, 14, 16]), 168);
     }
 }
